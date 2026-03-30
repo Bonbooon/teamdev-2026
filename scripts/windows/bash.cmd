00adfbbdce25e1d578ff58cc67be3e32805c271d
Bonbooon <akiyama.mamoru.69@gmail.com>
2026-03-30 10:50:52 +0900

    fix(windows): improve bash -lc argument forwarding

--- scripts/windows/bash.cmd
+++ scripts/windows/bash.cmd
@@ -3,21 +3,29 @@ setlocal
 
 set "GIT_BASH=C:\Program Files\Git\bin\bash.exe"
 if exist "%GIT_BASH%" (
-  "%GIT_BASH%" %*
-  exit /b %ERRORLEVEL%
+  goto :run
 )
 
 set "GIT_BASH=C:\Program Files\Git\usr\bin\bash.exe"
 if exist "%GIT_BASH%" (
-  "%GIT_BASH%" %*
-  exit /b %ERRORLEVEL%
+  goto :run
 )
 
 set "GIT_BASH=C:\Users\%USERNAME%\scoop\apps\git\current\bin\bash.exe"
 if exist "%GIT_BASH%" (
-  "%GIT_BASH%" %*
-  exit /b %ERRORLEVEL%
+  goto :run
 )
 
 echo Git Bash was not found. Install Git for Windows and ensure bash.exe is available. 1>&2
 exit /b 1
+
+:run
+if /I "%~1"=="-lc" (
+  shift
+  set "BASH_CMD=%*"
+  "%GIT_BASH%" -lc "%BASH_CMD%"
+  exit /b %ERRORLEVEL%
+)
+
+"%GIT_BASH%" %*
+exit /b %ERRORLEVEL%
